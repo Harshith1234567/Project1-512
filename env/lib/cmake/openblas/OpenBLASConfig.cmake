232f44bf167f12a256b64be282521fd409aa6533
Harshith S R <53342028+Harshith1234567@users.noreply.github.com>
2023-09-23 10:44:11 -0400

    Initial commit

---
+++ env/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.24")
+SET(OpenBLAS_INCLUDE_DIRS /Users/admin/Project1-512/env/include)
+SET(OpenBLAS_LIBRARIES /Users/admin/Project1-512/env/lib/libopenblas.dylib)
---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/admin/Project1-512/env/lib"
+XSLT_LIBS="-lxslt -L/Users/admin/Project1-512/env/lib -lxml2 "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/admin/Project1-512/env/include"
+MODULE_VERSION="xslt-1.1.37"
